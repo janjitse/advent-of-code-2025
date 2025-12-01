695c7287cb98a12a1cef7a0a4eb7caae0443de8d
Jan Jitse <janjitse@gmail.com>
2025-12-01 21:06:32 +0100

    Add template

---
+++ src/template.rs
@@ -0,0 +1,41 @@
+pub fn parse(contents: &str) -> Vec<i32> {
+    let vec: Vec<i32> = contents
+        .split_ascii_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+    vec
+}
+
+#[aoc(day1, part1)]
+pub fn part_a(contents: &str) -> i32 {
+    let vec = parse(contents);
+    vec.into_iter().sum()
+}
+
+#[aoc(day1, part2)]
+pub fn part_b(contents: &str) -> i32 {
+    let vec = parse(contents);
+    vec.into_iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{file, fs, path::Path};
+
+    #[test]
+    fn test_1() {
+        let s = Path::new(file!()).file_stem().unwrap().to_str().unwrap();
+        let file_path = format!("input/2025/{}_small.txt", s);
+        let contents = fs::read_to_string(file_path).expect("file not found");
+        assert_eq!(part_a(&contents), 0);
+    }
+
+    #[test]
+    fn test_2() {
+        let s = Path::new(file!()).file_stem().unwrap().to_str().unwrap();
+        let file_path = format!("input/2025/{}_small.txt", s);
+        let contents = fs::read_to_string(file_path).expect("file not found");
+        assert_eq!(part_b(&contents), 0);
+    }
+}
