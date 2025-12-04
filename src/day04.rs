b0811a89ee5fad30562c31f3b15383bf708808ce
Jan Jitse <janjitse@gmail.com>
2025-12-04 21:31:08 +0100

    Improved algorithm for day 4 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,19 +1,20 @@
+// use std::collections::HashMap;
 use crate::parsers::*;
-
+use rustc_hash::FxHashMap;
 
 #[aoc(day4, part1)]
 pub fn part_a(contents: &str) -> i32 {
     let mut accessible = 0;
     let vec = parse_array_of_chars(contents);
-    let directions: Vec<(usize, usize)> = vec![
+    let directions: [(usize, usize); 8] = [
         (0, 1),
+        (0, usize::MAX),
+        (1, usize::MAX),
         (1, 0),
         (1, 1),
         (usize::MAX, 0),
         (usize::MAX, 1),
         (usize::MAX, usize::MAX),
-        (1, usize::MAX),
-        (0, usize::MAX),
     ];
     for (row_idx, line) in vec.iter().enumerate() {
         for (col_idx, val) in line.iter().enumerate() {
@@ -27,73 +28,68 @@ pub fn part_a(contents: &str) -> i32 {
                     }
                     let test_c = vec[pos_y][pos_x];
                     if test_c == '@' {
-                        sur_papers +=1;
+                        sur_papers += 1;
                     }
-
                 }
                 if sur_papers < 4 {
                     accessible += 1;
                 }
             }
-            
-            
-
-        }       
+        }
     }
     accessible
-    
 }
 
 #[aoc(day4, part2)]
 pub fn part_b(contents: &str) -> i32 {
     let mut accessible = 0;
-    let mut vec = parse_array_of_chars(contents);
-    let directions: Vec<(usize, usize)> = vec![
+    let vec = parse_array_of_chars(contents);
+    let directions: [(usize, usize); 8] = [
         (0, 1),
+        (0, usize::MAX),
+        (1, usize::MAX),
         (1, 0),
         (1, 1),
         (usize::MAX, 0),
         (usize::MAX, 1),
         (usize::MAX, usize::MAX),
-        (1, usize::MAX),
-        (0, usize::MAX),
     ];
-    let mut changes = true;
-    while changes {
-        changes = false;
-        let mut new_vec = vec.clone();
-        for (row_idx, line) in vec.iter().enumerate() {
-            for (col_idx, val) in line.iter().enumerate() {
-                if *val == '@' {
-                    let mut sur_papers = 0;
-                    for dir in directions.iter() {
-                        let pos_x = col_idx.wrapping_add(dir.1);
-                        let pos_y = row_idx.wrapping_add(dir.0);
-                        if pos_x >= line.len() || pos_y >= vec.len() {
-                            continue;
-                        }
-                        let test_c = vec[pos_y][pos_x];
-                        if test_c == '@' {
-                            sur_papers +=1;
-                        }
-
+    let mut neighbors = FxHashMap::default();
+    for (row_idx, line) in vec.iter().enumerate() {
+        for (col_idx, val) in line.iter().enumerate() {
+            if *val == '@' {
+                let mut sur_papers = 0;
+                for dir in directions.iter() {
+                    let pos_x = col_idx.wrapping_add(dir.1);
+                    let pos_y = row_idx.wrapping_add(dir.0);
+                    if pos_x >= line.len() || pos_y >= vec.len() {
+                        continue;
                     }
-                    if sur_papers < 4 {
-                        accessible += 1;
-                        new_vec[row_idx][col_idx] = '.';
-                        changes = true;
+                    let test_c = vec[pos_y][pos_x];
+                    if test_c == '@' {
+                        sur_papers += 1;
                     }
                 }
-
-                
-                
-
+                neighbors.insert((row_idx, col_idx), sur_papers);
             }
         }
-        vec = new_vec;
     }
-
-    // vec.into_iter().sum()
+    let mut todo: Vec<(usize, usize)> = neighbors
+        .iter()
+        .filter(|&(_, val)| *val < 4)
+        .map(|(idx, _)| *idx)
+        .collect();
+    while let Some(point) = todo.pop() {
+        for dir in directions.iter() {
+            let new_point = (point.0.wrapping_add(dir.0), point.1.wrapping_add(dir.1));
+            let k = neighbors.entry(new_point).or_default();
+            *k -= 1;
+            if *k == 3 {
+                todo.push(new_point);
+            }
+        }
+        accessible += 1;
+    }
     accessible
 }
 
