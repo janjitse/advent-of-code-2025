8ca3a555c70c18cf20edc9267e2efbf566602a66
Jan Jitse <janjitse@gmail.com>
2025-12-07 16:58:05 +0100

    Some cleanup, shortening and micro-opts

--- src/day07.rs
+++ src/day07.rs
@@ -1,57 +1,55 @@
 use crate::parsers::parse_array_of_chars;
 
 #[aoc(day7, part1)]
-pub fn part_a(contents: &str) -> i32 {
+pub fn part_a(contents: &str) -> u64 {
     let vec = parse_array_of_chars(contents);
     let mut nr_splits = 0;
     let directions = [1, usize::MAX];
     let mut todo: Vec<bool> = vec![false; vec[0].len()];
 
-    for line in vec.iter() {
-        let mut new_todo = vec![false; vec[0].len()];
-        for (col_idx, pos) in line.iter().zip(todo.iter()).enumerate() {
+    for line in vec.into_iter() {
+        let mut new_todo = vec![false; line.len()];
+        for (col_idx, (pos, has_beam)) in line.iter().zip(todo.into_iter()).enumerate() {
             match pos {
-                ('S', _) => {
+                'S' => {
                     new_todo[col_idx] = true;
                 }
-                ('^', true) => {
-                    nr_splits += 1;
+                '^' => {
+                    nr_splits += has_beam as u64;
                     for dir in directions.iter() {
                         let new_beam = col_idx.wrapping_add(*dir);
                         if new_beam < line.len() {
-                            new_todo[new_beam] = true;
+                            new_todo[new_beam] |= has_beam;
                         }
                     }
                 }
-                (_, true) => {
-                    new_todo[col_idx] = true;
+                _ => {
+                    new_todo[col_idx] |= has_beam;
                 }
-                _ => {}
             }
         }
         todo = new_todo;
     }
-
     nr_splits
 }
 
 #[aoc(day7, part2)]
 pub fn part_b(contents: &str) -> u64 {
     let vec = parse_array_of_chars(contents);
-    let mut nr_splits = 0;
+    let mut nr_beams = 0;
     let directions = [1, usize::MAX];
     let mut todo: Vec<u64> = vec![0; vec[0].len()];
 
-    for line in vec.iter() {
-        let mut new_todo = vec![0; vec[0].len()];
-        for (col_idx, (pos, nr_tachyons)) in line.iter().zip(todo.iter()).enumerate() {
+    for line in vec.into_iter() {
+        let mut new_todo = vec![0; line.len()];
+        for (col_idx, (pos, nr_tachyons)) in line.iter().zip(todo.into_iter()).enumerate() {
             match pos {
                 'S' => {
                     new_todo[col_idx] += 1 + nr_tachyons;
-                    nr_splits += 1;
+                    nr_beams += 1;
                 }
                 '^' => {
-                    nr_splits += nr_tachyons;
+                    nr_beams += nr_tachyons;
                     for dir in directions.iter() {
                         let new_beam = col_idx.wrapping_add(*dir);
                         if new_beam < line.len() {
@@ -66,8 +64,7 @@ pub fn part_b(contents: &str) -> u64 {
         }
         todo = new_todo;
     }
-
-    nr_splits
+    nr_beams
 }
 
 #[cfg(test)]
