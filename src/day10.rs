eaaef25641f029a48f0b41c7b01b3d62f3424d30
Jan Jitse <janjitse@gmail.com>
2025-12-11 19:46:28 +0100

    Major cleanup

--- src/day10.rs
+++ src/day10.rs
@@ -14,7 +14,7 @@ pub fn parse(contents: &str) -> Vec<(Vec<bool>, Vec<Vec<u32>>, Vec<i64>)> {
         for toggle in s.iter().skip(1).take(s.len() - 2) {
             let toggle_digit = toggle
                 .chars()
-                .filter(|c| c.is_digit(10))
+                .filter(|c| c.is_ascii_digit())
                 .map(|x| x.to_digit(10).unwrap())
                 .collect::<Vec<u32>>();
             toggles.push(toggle_digit);
@@ -69,34 +69,10 @@ pub fn part_a(contents: &str) -> u64 {
         for t in powerset(&switches) {
             let t_len = t.len();
             let val = t.into_iter().fold(target, |x, y| x ^ y);
-            if val == 0 {
-                if t_len < min_length {
-                    min_length = t_len;
-                }
+            if val == 0 && t_len < min_length {
+                min_length = t_len;
             }
         }
-        output += min_length as u64;
-    }
-    output
-}
-
-// #[aoc(day10, part2)]
-#[allow(dead_code)]
-pub fn part_b(contents: &str) -> u64 {
-    let lines = parse(contents);
-    let mut output = 0;
-    for (idx, (_, toggles, joltage)) in lines.into_iter().enumerate() {
-        let mut switches = vec![];
-        for t in toggles.iter() {
-            let mut new_vector = vec![0; joltage.len()];
-            for v in t {
-                new_vector[*v as usize] = 1;
-            }
-            switches.push(new_vector);
-        }
-        switches.sort_unstable_by_key(|x| -x.iter().fold(0, |x, y| -x - y));
-        let min_length = recurse(switches, joltage).unwrap();
-        println!("Done line {:?}, min presses {:?}", idx, min_length);
         output += min_length as u64;
     }
     output
@@ -107,9 +83,6 @@ pub fn part_b_faster(contents: &str) -> i64 {
     let lines = parse(contents);
     let mut output = 0;
     for (idx, (_, toggles, joltage)) in lines.into_iter().enumerate() {
-        // if idx != 114 {
-        //     continue;
-        // }
         println!("Doing case {:?}", idx);
         let max_presses = *joltage.iter().max().unwrap();
         let mut switches = vec![];
@@ -158,26 +131,18 @@ pub fn part_b_faster(contents: &str) -> i64 {
         let intermed = recurse_b(row_reduced, target, solutions, max_presses).unwrap();
         println!("{:?}", intermed);
         output += intermed;
-        // let min_length = recurse(matrix_trans(&row_reduced), joltage).unwrap();
-        // output += min_length;
-        // let matrix = ndarray::Array2::from(switches);
-        // let min_length = recurse(switches, joltage).unwrap();
-        // let matrix = dmatrix![switches];
-        // println!("{:?}", matrix);
-        // println!("Done line {:?}, min presses {:?}", idx, min_length);
-        // output += min_length as u64;
     }
     output
 }
 
-fn vector_subtract(vec1: &Vec<i64>, vec2: &Vec<i64>, times: i64) -> Vec<i64> {
+fn vector_subtract(vec1: &[i64], vec2: &[i64], times: i64) -> Vec<i64> {
     vec1.iter()
         .zip(vec2.iter())
         .map(|(x, y)| *x - times * *y)
         .collect()
 }
 
-fn matrix_trans(matrix: &Vec<Vec<i64>>) -> Vec<Vec<i64>> {
+fn matrix_trans(matrix: &[Vec<i64>]) -> Vec<Vec<i64>> {
     let mut switches_transpose = vec![vec![0; matrix.len()]; matrix[0].len()];
     for idx_i in 0..matrix.len() {
         for idx_j in 0..matrix[0].len() {
@@ -187,20 +152,12 @@ fn matrix_trans(matrix: &Vec<Vec<i64>>) -> Vec<Vec<i64>> {
     switches_transpose
 }
 
-// fn identity_matrix(size: usize) -> Vec<Vec<i64>> {
-//     let mut output = vec![vec![0; size]; size];
-//     for idx in 0..size {
-//         output[idx][idx] = 1;
-//     }
-//     output
-// }
-
-fn gcd(a: &Vec<i64>) -> i64 {
+fn gcd(a: &[i64]) -> i64 {
     if a.len() == 2 {
         return egcd(a[0], a[1]).2;
     }
-    let gcd_rest = gcd(&a.iter().skip(1).copied().collect());
-    return gcd(&vec![a[0], gcd_rest]);
+    let gcd_rest = gcd(&a.iter().skip(1).copied().collect::<Vec<i64>>());
+    gcd(&[a[0], gcd_rest])
 }
 
 fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
@@ -213,10 +170,10 @@ fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
         (old_s, s) = (s, old_s - q * s);
         (old_t, t) = (t, old_t - q * t);
     }
-    return (old_s, old_t, old_r); // (x, y, gcd, such that x * a[0] + y * a[0] == gcd)
+    (old_s, old_t, old_r) // (x, y, gcd, such that x * a[0] + y * a[0] == gcd)
 }
 
-fn bezout(numbers: &Vec<i64>, mut target: i64, max: i64) -> Vec<Vec<i64>> {
+fn bezout(numbers: &[i64], mut target: i64, max: i64) -> Vec<Vec<i64>> {
     if numbers.is_empty() {
         return vec![vec![]];
     }
@@ -233,7 +190,7 @@ fn bezout(numbers: &Vec<i64>, mut target: i64, max: i64) -> Vec<Vec<i64>> {
     if target % common != 0 {
         return vec![vec![]];
     }
-    target = target / common;
+    target /= common;
     // println!("{:?}", reduced_numbers);
     if reduced_numbers.len() == 2 {
         let (x, y, gcd_v) = egcd(reduced_numbers[0], reduced_numbers[1]);
@@ -245,10 +202,7 @@ fn bezout(numbers: &Vec<i64>, mut target: i64, max: i64) -> Vec<Vec<i64>> {
             ]);
         }
         // println!("{:?}", pairs);
-        pairs = pairs
-            .into_iter()
-            .filter(|x| x.iter().all(|y| *y <= max && *y >= 0))
-            .collect();
+        pairs.retain(|x| x.iter().all(|y| *y <= max && *y >= 0));
         // println!("{:?}", pairs);
         return pairs;
     }
@@ -262,7 +216,7 @@ fn bezout(numbers: &Vec<i64>, mut target: i64, max: i64) -> Vec<Vec<i64>> {
                 .iter()
                 .copied()
                 .take(reduced_numbers.len() - 1)
-                .collect(),
+                .collect::<Vec<i64>>(),
             new_target,
             max,
         );
@@ -274,100 +228,39 @@ fn bezout(numbers: &Vec<i64>, mut target: i64, max: i64) -> Vec<Vec<i64>> {
             output.push(n_p);
         }
     }
-    return output;
+    output
 }
 
 fn row_reduce(
-    matrix: &Vec<Vec<i64>>,
+    matrix: &[Vec<i64>],
     pivot: usize,
     pivot_row_idx: usize,
-    par_matrix: &Vec<Vec<i64>>,
+    par_matrix: &[Vec<i64>],
 ) -> (Vec<Vec<i64>>, Vec<Vec<i64>>) {
-    let matrix_mut = matrix.clone();
-    let par_matrix_mut = par_matrix.clone();
-    let mut pivot_row = matrix_mut[pivot_row_idx].clone();
-    let mut pivot_row_par = par_matrix_mut[pivot_row_idx].clone();
+    let mut pivot_row = matrix[pivot_row_idx].clone();
+    let mut pivot_row_par = par_matrix[pivot_row_idx].clone();
     if pivot_row[pivot] < 0 {
-        pivot_row = pivot_row.into_iter().map(|x| -1 * x).collect();
-        pivot_row_par = pivot_row_par.into_iter().map(|x| -1 * x).collect();
+        pivot_row = pivot_row.into_iter().map(|x| -x).collect();
+        pivot_row_par = pivot_row_par.into_iter().map(|x| -x).collect();
     }
 
     // println!("Pivot row: {:?}", pivot_row);
     let mut new_matrix = vec![];
     let mut new_par_matrix = vec![];
-    for (idx, (row, par_row)) in matrix_mut
-        .into_iter()
-        .zip(par_matrix_mut.into_iter())
-        .enumerate()
-    {
+    for (idx, (row, par_row)) in matrix.iter().zip(par_matrix.iter()).enumerate() {
         if idx < pivot_row_idx {
-            new_matrix.push(row);
-            new_par_matrix.push(par_row);
+            new_matrix.push(row.clone());
+            new_par_matrix.push(par_row.clone());
         } else if idx == pivot_row_idx {
             new_matrix.push(pivot_row.clone());
             new_par_matrix.push(pivot_row_par.clone());
         } else {
             let div = row[pivot] / pivot_row[pivot];
-            let mut new_row = vector_subtract(&row, &pivot_row, div);
+            let mut new_row = vector_subtract(row, &pivot_row, div);
             let first_non_zero = new_row.iter().position(|x| *x != 0);
-            let mut par_new_row = vector_subtract(&par_row, &pivot_row_par, div);
+            let mut par_new_row = vector_subtract(par_row, &pivot_row_par, div);
 
-            match first_non_zero {
-                Some(pos) => {
-                    if new_row[pos] != 1 {
-                        let div = new_row[pos];
-                        if new_row.iter().all(|x| *x % div == 0)
-                            && par_new_row.iter().all(|x| *x % div == 0)
-                        {
-                            new_row = new_row.into_iter().map(|x| x / div).collect();
-                            par_new_row = par_new_row.into_iter().map(|x| x / div).collect();
-                        }
-                    }
-                    if new_row[pos] < 0 {
-                        new_row = new_row.into_iter().map(|x| x * -1).collect();
-                        par_new_row = par_new_row.into_iter().map(|x| x * -1).collect();
-                    }
-                }
-                None => {}
-            };
-            new_matrix.push(new_row);
-            new_par_matrix.push(par_new_row);
-        }
-    }
-
-    return (new_matrix, new_par_matrix);
-}
-
-fn row_reduce_updown(
-    matrix: &Vec<Vec<i64>>,
-    pivot: usize,
-    pivot_row_idx: usize,
-    par_matrix: &Vec<Vec<i64>>,
-) -> (Vec<Vec<i64>>, Vec<Vec<i64>>) {
-    let matrix_mut = matrix.clone();
-    let par_matrix_mut = par_matrix.clone();
-    let mut pivot_row = matrix_mut[pivot_row_idx].clone();
-    let mut pivot_row_par = par_matrix_mut[pivot_row_idx].clone();
-    if pivot_row[pivot] < 0 {
-        pivot_row = pivot_row.into_iter().map(|x| -1 * x).collect();
-        pivot_row_par = pivot_row_par.into_iter().map(|x| -1 * x).collect();
-    }
-
-    // println!("Pivot row: {:?}", pivot_row);
-    let mut new_matrix = vec![];
-    let mut new_par_matrix = vec![];
-    for (_idx, (row, par_row)) in matrix_mut
-        .into_iter()
-        .zip(par_matrix_mut.into_iter())
-        .enumerate()
-    {
-        let div = row[pivot] / pivot_row[pivot];
-        let mut new_row = vector_subtract(&row, &pivot_row, div);
-        let first_non_zero = new_row.iter().position(|x| *x != 0);
-        let mut par_new_row = vector_subtract(&par_row, &pivot_row_par, div);
-
-        match first_non_zero {
-            Some(pos) => {
+            if let Some(pos) = first_non_zero {
                 if new_row[pos] != 1 {
                     let div = new_row[pos];
                     if new_row.iter().all(|x| *x % div == 0)
@@ -378,34 +271,67 @@ fn row_reduce_updown(
                     }
                 }
                 if new_row[pos] < 0 {
-                    new_row = new_row.into_iter().map(|x| x * -1).collect();
-                    par_new_row = par_new_row.into_iter().map(|x| x * -1).collect();
+                    new_row = new_row.into_iter().map(|x| -x).collect();
+                    par_new_row = par_new_row.into_iter().map(|x| -x).collect();
+                }
+            };
+            new_matrix.push(new_row);
+            new_par_matrix.push(par_new_row);
+        }
+    }
+
+    (new_matrix, new_par_matrix)
+}
+
+fn row_reduce_updown(
+    matrix: &[Vec<i64>],
+    pivot: usize,
+    pivot_row_idx: usize,
+    par_matrix: &[Vec<i64>],
+) -> (Vec<Vec<i64>>, Vec<Vec<i64>>) {
+    let mut pivot_row = matrix[pivot_row_idx].clone();
+    let mut pivot_row_par = par_matrix[pivot_row_idx].clone();
+    if pivot_row[pivot] < 0 {
+        pivot_row = pivot_row.into_iter().map(|x| -x).collect();
+        pivot_row_par = pivot_row_par.into_iter().map(|x| -x).collect();
+    }
+
+    // println!("Pivot row: {:?}", pivot_row);
+    let mut new_matrix = vec![];
+    let mut new_par_matrix = vec![];
+    for (row, par_row) in matrix.iter().zip(par_matrix.iter()) {
+        let div = row[pivot] / pivot_row[pivot];
+        let mut new_row = vector_subtract(row, &pivot_row, div);
+        let first_non_zero = new_row.iter().position(|x| *x != 0);
+        let mut par_new_row = vector_subtract(par_row, &pivot_row_par, div);
+
+        if let Some(pos) = first_non_zero {
+            if new_row[pos] != 1 {
+                let div = new_row[pos];
+                if new_row.iter().all(|x| *x % div == 0)
+                    && par_new_row.iter().all(|x| *x % div == 0)
+                {
+                    new_row = new_row.into_iter().map(|x| x / div).collect();
+                    par_new_row = par_new_row.into_iter().map(|x| x / div).collect();
                 }
             }
-            None => {}
+            if new_row[pos] < 0 {
+                new_row = new_row.into_iter().map(|x| -x).collect();
+                par_new_row = par_new_row.into_iter().map(|x| -x).collect();
+            }
         };
         new_matrix.push(new_row);
         new_par_matrix.push(par_new_row);
     }
 
-    return (new_matrix, new_par_matrix);
+    (new_matrix, new_par_matrix)
 }
-
-// fn matrix_vec_mul(matrix: &Vec<Vec<i64>>, vector: &Vec<i64>) -> Vec<i64> {
-//     let mut output = vec![0; matrix.len()];
-//     for (idx, l) in matrix.iter().enumerate() {
-//         output[idx] = l.iter().zip(vector).fold(0, |v, (x, y)| v + *x * *y);
-//     }
-//     output
-// }
 
 fn smith_normal_form(switches: Vec<Vec<i64>>, joltage: Vec<i64>) -> (Vec<Vec<i64>>, Vec<Vec<i64>>) {
     // Choose pivot: smallest column index with non-zero entry
     let mut switches_transpose = matrix_trans(&switches);
-    // let mut switches_transpose = switches.clone();
     let mut prev_pivot_row = 0;
     let mut prev_pivot_col = 0;
-    // let mut t_matrix = identity_matrix(switches_transpose.len());
     let mut t_matrix = vec![];
     for x in joltage.iter() {
         t_matrix.push(vec![*x]);
@@ -430,21 +356,18 @@ fn smith_normal_form(switches: Vec<Vec<i64>>, joltage: Vec<i64>) -> (Vec<Vec<i64
         if min_col == switches_transpose[0].len() {
             break;
         }
-        // println!("pivot found: row{:?}, col {:?}", cur_row, min_col);
         switches_transpose.swap(cur_row, prev_pivot_row);
         // println!("switched {:?}", switches_transpose);
         t_matrix.swap(cur_row, prev_pivot_row);
         (switches_transpose, t_matrix) =
             row_reduce(&switches_transpose, min_col, prev_pivot_row, &t_matrix);
         prev_pivot_col = min_col;
-        prev_pivot_row = prev_pivot_row + 1;
+        prev_pivot_row += 1;
     }
     // println!("reduced: {:?}", &switches_transpose);
     // println!("reduced transposes: {:?}", matrix_trans(&switches_transpose));
     // println!("T matrix: {:?}", t_matrix);
-    // Improve pivot, use Bezout?
-    // Eliminate entries
-    return (switches_transpose, t_matrix);
+    (switches_transpose, t_matrix)
 }
 
 fn recurse_b(
@@ -508,57 +431,7 @@ fn recurse_b(
         }
     }
 
-    return min_presses;
-}
-
-fn recurse(mut switches: Vec<Vec<i64>>, mut remaining: Vec<i64>) -> Option<i64> {
-    if remaining.iter().all(|x| *x == 0) {
-        return Some(0);
-    }
-    if remaining.iter().any(|x| *x < 0) {
-        return None;
-    }
-    if switches.is_empty() {
-        return None;
-    }
-    let remaining_non_zero: Vec<usize> = remaining
-        .iter()
-        .enumerate()
-        .filter(|(_idx, x)| **x > 0)
-        .map(|(idx, _)| idx)
-        .collect();
-    let first_remaining = remaining_non_zero[0];
-    let mut vec_to_take = 0;
-    for (idx, s) in switches.iter().enumerate() {
-        if s[first_remaining] > 0 {
-            vec_to_take = idx;
-            break;
-        }
-    }
-    // if switches[vec_to_take] {}
-    let test_vec = switches.remove(vec_to_take);
-
-    let mut min_presses = i64::MAX;
-    let to_remove = remaining[first_remaining];
-    for presses in 0..=to_remove {
-        let cur_pres = recurse(switches.clone(), remaining.clone());
-        match cur_pres {
-            Some(number) => {
-                min_presses = min_presses.min(presses + number);
-            }
-            None => {}
-        }
-        for (idx, val) in test_vec.iter().enumerate() {
-            remaining[idx] -= *val;
-        }
-        if remaining.iter().any(|x| *x < 0) {
-            break;
-        }
-    }
-    if min_presses == i64::MAX {
-        return None;
-    }
-    return Some(min_presses);
+    min_presses
 }
 
 #[cfg(test)]
